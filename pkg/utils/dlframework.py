2534794ff58f97d694f474c8083c966ab9c31b35
Chienping Tsung <jianpingzong@outlook.com>
2025-06-07 14:33:49 +0800

    dlframework

--- pkg/utils/dlframework.py
+++ pkg/utils/dlframework.py
@@ -3,5 +3,5 @@ import torch
 
 def get_device(force_cpu=False):
     device = torch.device('cuda' if torch.cuda.is_available() and not force_cpu else 'cpu')
-    print(f'Using {device} device. (Notice: {torch.cuda.device_count()} cuda device available.)')
+    print(f'Using {device} device (Notice: {torch.cuda.device_count()} cuda device available)')
     return device
