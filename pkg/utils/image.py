f27c5f8b214ee595c5ecd63805550595a26824ce
Chienping Tsung <jianpingzong@outlook.com>
2025-06-07 15:26:47 +0800

    image

--- pkg/utils/image.py
+++ pkg/utils/image.py
@@ -11,11 +11,11 @@ def pil_to_cv(image):
     return cv2.cvtColor(np.asarray(image), cv2.COLOR_RGB2BGR)
 
 
-def add_box_mask(image, box=None, mask=None):
-    if mask is not None:
-        overlay = Image.new('RGB', image.size, (255, 0, 0))
-        image = Image.composite(overlay, image, mask)
+def draw_box_mask(image, box=None, mask=None):
     if box is not None:
         draw = ImageDraw.Draw(image)
         draw.rectangle(box, outline='red', width=5)
+    if mask is not None:
+        overlay = Image.new('RGB', image.size, (255, 0, 0))
+        image = Image.composite(overlay, image, mask)
     return image
